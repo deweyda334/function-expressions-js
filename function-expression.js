9226fb4bb41757a7cf9ca7a6761dfe6a90633965
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-05-06 14:12:52 -0400

    Edited our JavaScript file

--- function-expression.js
+++ function-expression.js
@@ -5,54 +5,64 @@
 // Assign function to a variable named calcSum
 // In other words, our function has no name to start with so we
 // are using the variable name (calcSum) as the function name
-const calcSum = function(num1, num2){
-    return num1 + num2;
-}
+// const calcSum = function(num1, num2){
+//     return num1 + num2;
+// }
 
 // Find the sum by calling the function
 // Result of calculation is returned from the function and
 // stored in a NEW variable named answer
-const answer = calcSum(12, 5);
+// const answer = calcSum(12, 5);
 // Option 1
 // Displaying the result of the calculation in the browser console
-console.log(`(Option 1): The sum is: ${answer}`);
+// console.log(`(Option 1): The sum is: ${answer}`);
 // Option 2
 // Calling the function calcSum from within the template string
-console.log(`(Option 2): The sum is: ${calcSum(20, 4)}`);
+// console.log(`(Option 2): The sum is: ${calcSum(20, 4)}`);
 // Option 3
 // Calling the function calcSum from within the template string
-const firstNum = 20;
-const secondNum = 4;
-console.log(`(Option 3): The sum is: ${calcSum(firstNum, secondNum)}`);
+// const firstNum = 20;
+// const secondNum = 4;
+// console.log(`(Option 3): The sum is: ${calcSum(firstNum, secondNum)}`);
 
 // My function rewritten as a FUNCTION EXPRESSION
 // FUNCTION #2
 
-function welcomeUser (fName) {
-    // Welcome the user by their first name
-    return `Welcome to Career Tech, ${fName}!`;
-}
+// function welcomeUser (fName) {
+//     // Welcome the user by their first name
+//     return `Welcome to Career Tech, ${fName}!`;
+// }
 // Call the welcomeUser function and use the alert ( ) method to display the welcome message in an alert box
-const myName = 'Dax';
-alert(welcomeUser(myName));
-
-
-
-
-
-
-
+// const myName = 'Dax';
+// alert(welcomeUser(myName));
 
 // My function rewritten as a FUNCTION EXPRESSION
 // FUNCTION #3
-function showBiography (fName, lName, userAge) {
-    // Build a template string containing the user's full name and age
-    return `Hello! My name is ${fName} ${lName} and I am ${userAge} years old.`;
-}
+// function showBiography (fName, lName, userAge) {
+//     // Build a template string containing the user's full name and age
+//     return `Hello! My name is ${fName} ${lName} and I am ${userAge} years old.`;
+// }
 // Call the showBiography function and display the info
 // about the user in the browser console
+
+// const showBiography = function (fName, lName, userAge) {
+//     return `Hello! My name is ${fName} ${lName} and I am ${userAge} years old.`;
+// }
+
+// const firstName = 'Dax';
+// const lastName = 'Dewey';
+// const age = 18;
+// console.log(showBiography(firstName, lastName, age));
+
+// function welcomeUser (fName) {
+//     // Welcome the user by their first name
+//     return `Welcome to Career Tech, ${fName}!`;
+// }
+
+const welcomeUser = function(fName) {
+    return `Welcome to Career Tech, ${fName}!`;
+}
+// Call the welcomeUser function and use the alert ( ) method to display the welcome message in an alert box
 const firstName = 'Dax';
-const lastName = 'Dewey';
-const age = 18;
-console.log(showBiography(firstName, lastName, age));
+alert(welcomeUser(firstName));
 
